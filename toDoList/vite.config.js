c92c7c45d5bf3e91419d239e5e4e3d6cbdbdceca
Abdul Basit <ab1438068@gmail.com>
2023-12-25 22:33:56 +0500

    Add GitHub Pages configuration

--- toDoList/vite.config.js
+++ toDoList/vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],build: {
+  plugins: [react()],
+  build: {
     outDir: 'docs', // Set your custom output directory
   },
+  base: '/ToDoList/',
 })
